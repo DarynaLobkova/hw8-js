4bb90b167764395ec2752f9e1ad4858a0977622e
Daryna Lobkova <darinka021193@gmail.com>
2022-11-24 21:31:14 +0100

    update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,10 @@ import images from "./app.js";
 const refs = {
   gallery: document.querySelector(".js-gallery"),
   image: document.createElement("img"),
-  lightbox: document.querySelector(".lightbox"),
+ lightbox: document.querySelector(".lightbox"),
+ closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector(".lightbox__content"),
+  lightbox__image: document.querySelector(".lightbox__image"),
 
 };
 
@@ -30,3 +33,28 @@ const galleryMarkup = images.reduce(
 refs.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
 refs.image.classList.add("gallery__image");
 
+
+refs.gallery.addEventListener('click', onImageClick)
+
+function onImageClick(e) {
+     e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const largeImageURL = e.target.dataset.source;
+
+    refs.lightbox.classList.add('is-open')
+
+   setLargeImage(largeImageURL)
+    
+    
+}
+function setLargeImage(url) {
+    refs.lightbox__image.src = url;
+}
+function closeModal() {
+      refs.lightbox.classList.remove('is-open')
+    
+}
+
+refs.closeModalBtn.addEventListener('click', closeModal)
